a6ef8148fe5a90018b20b780edc966ce3117c816
Gaurav Singh <grvsgh7@gmail.com>
2021-06-30 20:48:20 +0530

    Assignment Completed

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import * as Speech from 'expo-speech';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    inputWords: ''
+  }
+  changeWords = (text) => {
+    this.setState({ inputWords: text });
+  }
+  speak = () => {
+    const words = this.state.inputWords;
+    Speech.speak(words);
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.headingText}>Text To Speech</Text>
+        </View>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Write Some Words"
+          value={this.state.words}
+          onChangeText={(text) => this.changeWords(text)}
+        />
+        <Button title="Say Something" onPress={this.speak} />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +40,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputText: {
+    borderStyle: 'solid',
+    padding: 10,
+    marginBottom: 10,
+    backgroundColor: '#DEFEA7',
+    width: 300
+  },
+  headingText: {
+    marginBottom: 10,
+    fontWeight: 'bold',
+    fontSize: 25
+  }
 });
